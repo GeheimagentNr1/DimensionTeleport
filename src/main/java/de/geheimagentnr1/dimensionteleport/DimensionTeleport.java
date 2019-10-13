07954fb2e009957009617bac99bbd1d695014512
GeheimagentNr1 <GeheimeLache@hotmail.com>
2019-10-13 19:05:10 +0200

    Dimension Teleport funktioniert jetzt auf der 1.14.4

---
+++ src/main/java/de/geheimagentnr1/dimensionteleport/DimensionTeleport.java
@@ -0,0 +1,10 @@
+package de.geheimagentnr1.dimensionteleport;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod( DimensionTeleport.MODID )
+public class DimensionTeleport {
+	
+	public final static String MODID = "dimensionteleport";
+}
