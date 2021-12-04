2645c5abfd8fab63d39117f1f36b09582d6dcecd
GeheimagentNr1 <geheimelache@hotmail.com>
2021-12-04 17:21:41 +0100

    Ported to 1.18

--- src/main/java/de/geheimagentnr1/dimensionteleport/DimensionTeleport.java
+++ src/main/java/de/geheimagentnr1/dimensionteleport/DimensionTeleport.java
@@ -3,7 +3,7 @@ package de.geheimagentnr1.dimensionteleport;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+import net.minecraftforge.network.NetworkConstants;
 
 
 @SuppressWarnings( "UtilityClassWithPublicConstructor" )
@@ -18,7 +18,7 @@ public class DimensionTeleport {
 		ModLoadingContext.get().registerExtensionPoint(
 			IExtensionPoint.DisplayTest.class,
 			() -> new IExtensionPoint.DisplayTest(
-				() -> FMLNetworkConstants.IGNORESERVERONLY,
+				() -> NetworkConstants.IGNORESERVERONLY,
 				( remote, isServer ) -> true
 			)
 		);
