bd918a958fb4da71c122c67d7ab3d1443eb7bb9e
GeheimagentNr1 <geheimelache@hotmail.com>
2022-07-19 13:05:50 +0200

    Update forge version

--- src/main/java/de/geheimagentnr1/dimensionteleport/DimensionTeleport.java
+++ src/main/java/de/geheimagentnr1/dimensionteleport/DimensionTeleport.java
@@ -1,26 +1,11 @@
 package de.geheimagentnr1.dimensionteleport;
 
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
 
 
-@SuppressWarnings( "UtilityClassWithPublicConstructor" )
 @Mod( DimensionTeleport.MODID )
 public class DimensionTeleport {
 	
 	
 	public static final String MODID = "dimensionteleport";
-	
-	public DimensionTeleport() {
-		
-		ModLoadingContext.get().registerExtensionPoint(
-			IExtensionPoint.DisplayTest.class,
-			() -> new IExtensionPoint.DisplayTest(
-				() -> NetworkConstants.IGNORESERVERONLY,
-				( remote, isServer ) -> true
-			)
-		);
-	}
 }
