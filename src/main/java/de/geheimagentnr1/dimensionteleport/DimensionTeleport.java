7fa31ecb06ea6e949932fd5d373c43ecb90614ff
GeheimagentNr1 <geheimelache@hotmail.com>
2023-03-21 19:50:20 +0100

    Current Status of Minecraft Forge API

--- src/main/java/de/geheimagentnr1/dimensionteleport/DimensionTeleport.java
+++ src/main/java/de/geheimagentnr1/dimensionteleport/DimensionTeleport.java
@@ -1,11 +1,24 @@
 package de.geheimagentnr1.dimensionteleport;
 
+import de.geheimagentnr1.dimensionteleport.handlers.ForgeEventHandler;
+import de.geheimagentnr1.minecraft_forge_api.AbstractMod;
 import net.minecraftforge.fml.common.Mod;
 
 
 @Mod( DimensionTeleport.MODID )
-public class DimensionTeleport {
+public class DimensionTeleport extends AbstractMod {
 	
 	
-	public static final String MODID = "dimensionteleport";
+	static final String MODID = "dimensionteleport";
+	
+	public DimensionTeleport() {
+		
+		super( MODID, 1, "" );
+	}
+	
+	@Override
+	protected void initMod() {
+	
+		registerForgeEventHandler( new ForgeEventHandler() );
+	}
 }
--- src/main/java/de/geheimagentnr1/dimensionteleport/handlers/ForgeEventHandler.java
+++ src/main/java/de/geheimagentnr1/dimensionteleport/handlers/ForgeEventHandler.java
@@ -1,18 +1,17 @@
 package de.geheimagentnr1.dimensionteleport.handlers;
 
-import de.geheimagentnr1.dimensionteleport.DimensionTeleport;
 import de.geheimagentnr1.dimensionteleport.elements.commands.dimension_teleport.DimensionTeleportCommand;
+import de.geheimagentnr1.minecraft_forge_api.events.ForgeEventHandlerInterface;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
 
-@Mod.EventBusSubscriber( modid = DimensionTeleport.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE )
-public class ForgeEventHandler {
+public class ForgeEventHandler implements ForgeEventHandlerInterface {
 	
 	
 	@SubscribeEvent
-	public static void handlerRegisterCommandsEvent( RegisterCommandsEvent event ) {
+	@Override
+	public void handlerRegisterCommandsEvent( RegisterCommandsEvent event ) {
 		
 		DimensionTeleportCommand.register( event.getDispatcher() );
 	}
